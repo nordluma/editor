7ba2d39d7dcab97dadda75f5effa1554280afa63
nordluma <maxnordlund.95@hotmail.com>
2024-02-27 23:37:40 +0200

    feat: change `Sandbox` into an `Application`

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use iced::{
-    widget::{column, container, text, text_editor},
-    Sandbox, Settings,
+    executor,
+    widget::{column, container, horizontal_space, row, text, text_editor},
+    Application, Command, Length, Settings, Theme,
 };
 
 fn main() -> iced::Result {
@@ -16,25 +17,33 @@ struct Editor {
     content: text_editor::Content,
 }
 
-impl Sandbox for Editor {
+impl Application for Editor {
     type Message = Messages;
+    type Theme = Theme;
+    type Executor = executor::Default;
+    type Flags = ();
 
-    fn new() -> Self {
-        Self {
-            content: text_editor::Content::with(include_str!("main.rs")),
-        }
+    fn new(_flags: Self::Flags) -> (Self, Command<Messages>) {
+        (
+            Self {
+                content: text_editor::Content::with(include_str!("main.rs")),
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
         String::from("An iced out editor")
     }
 
-    fn update(&mut self, message: Self::Message) {
+    fn update(&mut self, message: Self::Message) -> Command<Messages> {
         match message {
             Messages::Edit(action) => {
                 self.content.edit(action);
             }
         }
+
+        Command::none()
     }
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
@@ -44,7 +53,11 @@ impl Sandbox for Editor {
             text(format!("{}:{}", line + 1, column + 1))
         };
 
-        container(column![input, position]).padding(10).into()
+        let status_bar = row![horizontal_space(Length::Fill), position];
+
+        container(column![input, status_bar].spacing(10))
+            .padding(10)
+            .into()
     }
 
     fn theme(&self) -> iced::Theme {
