74e9750adbed885cb5b43b5fde36791d60b03561
nordluma <maxnordlund.95@hotmail.com>
2024-02-27 23:09:38 +0200

    feat: hello world widget

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{widget::text, Sandbox, Settings};
+
+fn main() -> iced::Result {
+    Editor::run(Settings::default())
+}
+
+#[derive(Debug)]
+enum Messages {}
+
+struct Editor;
+
+impl Sandbox for Editor {
+    type Message = Messages;
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn title(&self) -> String {
+        String::from("An iced out editor")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {}
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        text("Hello, iced").into()
+    }
 }
